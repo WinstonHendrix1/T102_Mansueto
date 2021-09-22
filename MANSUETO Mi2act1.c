8075bae4299e75eec8a886ecd3833288afa6ea62
WinstonHendrix1 <89184668+WinstonHendrix1@users.noreply.github.com>
2021-09-22 23:41:59 +0800

    Add files via upload

---
+++ MANSUETO Mi2act1.c
@@ -0,0 +1,12 @@
+//Winston Hendrix Mansueto
+#include <stdio.h>
+
+int main()
+{
+    //declaration of variable
+    int age = 19;
+    //printing the variable
+    printf("I'm Winston Hendrix Mansueto, Age of %d years old", age);
+
+    return 0;
+}
